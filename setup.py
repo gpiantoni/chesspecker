9a196e067408da8552b06b91bcb7092f42414384
Gio on ArchXPS <github@gpiantoni.com>
2022-05-15 21:40:46 +0200

    chess pecker

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='chesspecker',
+    version='0.2',
+    packages=find_packages(exclude=('test', )),
+    install_requires=[
+        'chess',
+        ],
+    entry_points={
+        'console_scripts': [
+            'peckerchess=chesspecker.bin:main',
+        ],
+    },
+)
